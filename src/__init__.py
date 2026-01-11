a7b63990064bf15873c27b852da9044132e53e64
MASAMICHI IIZUMI <m.iizumi@miosync.email>
2026-01-11 13:01:45 +0900

    Initialize BCT module with documentation
    Add module documentation and versioning information.

---
+++ src/__init__.py
@@ -0,0 +1,16 @@
+"""
+Binary Convolution Theory (BCT)
+===============================
+
+A structural approach to studying the multiplicative structure of integers 
+through their binary representations.
+
+Modules:
+    - core: Foundational definitions (H, C, L invariants)
+    - theorems: Verification code for main theorems
+    - lemmas: Supporting lemmas
+"""
+
+from . import core
+
+__version__ = '0.1.0'
